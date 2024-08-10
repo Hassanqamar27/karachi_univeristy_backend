59809e332502788acfb84582a3a4a6d7702a0eeb
hassanqamer27 <hassanqamer2002@gmail.com>
2024-08-11 04:01:11 +0500

    mongodb

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ import mongoose from "mongoose";
 // import { DB_NAME } from "./src/utils/constants.js";
 import express from "express";
 import dotenv from "dotenv";
-import studentRoutes from "./src/routes/studentRoutes.js";
-import teacherRoutes from "./src/routes/teacherRoutes.js";
-import adminRoutes from "./src/routes/adminRoutes.js";
-import assignmentRoutes from "./src/routes/router.js";
-import router from "./src/routes/studentRoutes.js";
+// import studentRoutes from "./src/routes/studentRoutes.js";
+// import teacherRoutes from "./src/routes/teacherRoutes.js";
+// import adminRoutes from "./src/routes/adminRoutes.js";
+// import assignmentRoutes from "./src/routes/router.js";
+import router from "./src/routes/router.js";
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use("/api", router);
+
 (async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI); //(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    const uri = process.env.MONGODB_URI;
+    await mongoose
+      .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log("MongoDB connected!"))
+      .catch((error) => console.log("err mongodb", error.message));
+
     app.on("error", (error) => {
       console.log("error", error);
       throw error;
